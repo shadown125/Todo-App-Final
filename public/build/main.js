f4e2549613dee8856424ed620dbef2f94b7b5e77
YoLoN <shadyolown13@onet.pl>
2021-06-19 22:01:21 +0200

    Fixed functionality of client-side validation in register form

--- public/build/main.js
+++ public/build/main.js
@@ -41,16 +41,16 @@ var PreventInvalidFormSubmissions = /*#__PURE__*/function () {
     _classCallCheck(this, PreventInvalidFormSubmissions);
 
     this.todoRegisterForm = jquery__WEBPACK_IMPORTED_MODULE_4___default()('.todo-user-register');
+    this.invalidInput = true;
 
     if (this.todoRegisterForm[0] !== undefined) {
-      this.invalidInput = false;
       this.todoRegisterForm[0].addEventListener('change', this.todoRegister.bind(this));
       this.todoRegisterForm.on('click', function (event) {
-        if (!_this.invalidInput) {
+        if (_this.invalidInput) {
           event.preventDefault();
           event.stopPropagation();
 
-          _this.todoRegister().bind(_this);
+          _this.todoRegister();
         }
       });
     }
@@ -62,57 +62,65 @@ var PreventInvalidFormSubmissions = /*#__PURE__*/function () {
       var _this2 = this;
 
       var firstPassword;
+      var validInputs = [];
       this.todoRegisterForm[0].forEach(function (element, index) {
         if (element.name === "todo_user[name]" && element.value.length < 3) {
           element.classList.add("is-invalid");
           element.nextElementSibling.style.display = 'block';
           element.nextElementSibling.innerHTML = "This Name Need atleast 3 characters";
-          _this2.invalidInput = false;
+          validInputs.push(0);
         } else if (element.name === "todo_user[name]" && element.value.length >= 3) {
           element.classList.remove("is-invalid");
           element.nextElementSibling.style.display = 'none';
           element.nextElementSibling.innerHTML = "";
-          _this2.invalidInput = true;
+          validInputs.push(1);
         }
 
         if (element.name === "todo_user[email]" && !element.value.includes('@') && !element.value.includes('.')) {
           element.classList.add("is-invalid");
           element.nextElementSibling.style.display = 'block';
           element.nextElementSibling.innerHTML = "The E-Mail is not valid";
-          _this2.invalidInput = false;
+          validInputs.push(0);
         } else if (element.name === "todo_user[email]" && element.value.includes('@') && element.value.includes('.')) {
           element.classList.remove("is-invalid");
           element.nextElementSibling.style.display = 'none';
           element.nextElementSibling.innerHTML = "";
-          _this2.invalidInput = true;
+          validInputs.push(1);
         }
 
         if (element.name === "todo_user[password][first]" && element.value.length < 6) {
           element.classList.add("is-invalid");
           element.nextElementSibling.style.display = 'block';
           element.nextElementSibling.innerHTML = "Your password must contains atleast 6 characters";
-          _this2.invalidInput = false;
           firstPassword = element.value;
+          validInputs.push(0);
         } else if (element.name === "todo_user[password][first]" && element.value.length >= 6) {
           firstPassword = element.value;
           element.classList.remove("is-invalid");
           element.nextElementSibling.style.display = 'none';
           element.nextElementSibling.innerHTML = "";
-          _this2.invalidInput = true;
+          validInputs.push(1);
         }
 
         if (element.name === "todo_user[password][second]") {
-          if (element.value === firstPassword) {
+          if (element.value === firstPassword && firstPassword.length >= 6) {
             element.classList.remove("is-invalid");
             element.nextElementSibling.style.display = 'none';
             element.nextElementSibling.innerHTML = "";
-            _this2.invalidInput = true;
+            validInputs.push(1);
           } else if (firstPassword !== '') {
-            _this2.invalidInput = false;
             element.classList.add("is-invalid");
             element.nextElementSibling.style.display = 'block';
             element.nextElementSibling.innerHTML = "Your repeated password don't match your password. Please try again";
+            validInputs.push(0);
           }
+        }
+      });
+      validInputs.forEach(function (element) {
+        if (element === 0) {
+          _this2.invalidInput = true;
+        } else {
+          _this2.invalidInput = false;
         }
       });
     }
--- public/src/scripts/PreventInvalidFormSubmissions.js
+++ public/src/scripts/PreventInvalidFormSubmissions.js
@@ -3,15 +3,14 @@ import $ from 'jquery'
 export default class PreventInvalidFormSubmissions {
     constructor() {
         this.todoRegisterForm = $('.todo-user-register');
-
+        this.invalidInput = true;
         if (this.todoRegisterForm[0] !== undefined) {
-            this.invalidInput = false;
             this.todoRegisterForm[0].addEventListener('change', this.todoRegister.bind(this));
             this.todoRegisterForm.on('click', (event) => {
-                if(!this.invalidInput) {
+                if(this.invalidInput) {
                     event.preventDefault();
                     event.stopPropagation();
-                    this.todoRegister().bind(this);
+                    this.todoRegister();
                 }
             })
         }
@@ -19,57 +18,65 @@ export default class PreventInvalidFormSubmissions {
 
     todoRegister() {
         let firstPassword;
+        let validInputs = []
         this.todoRegisterForm[0].forEach((element, index) => {
             if (element.name === "todo_user[name]" && element.value.length < 3) {
                 element.classList.add("is-invalid")
                 element.nextElementSibling.style.display = 'block';
                 element.nextElementSibling.innerHTML = "This Name Need atleast 3 characters";
-                this.invalidInput = false;
+                validInputs.push(0)
             } else if (element.name === "todo_user[name]" && element.value.length >= 3) {
                 element.classList.remove("is-invalid")
                 element.nextElementSibling.style.display = 'none';
                 element.nextElementSibling.innerHTML = "";
-                this.invalidInput = true;
+                validInputs.push(1)
             }
 
             if (element.name === "todo_user[email]"&& !element.value.includes('@') && !element.value.includes('.')) {
                 element.classList.add("is-invalid")
                 element.nextElementSibling.style.display = 'block';
                 element.nextElementSibling.innerHTML = "The E-Mail is not valid";
-                this.invalidInput = false;
+                validInputs.push(0)
             } else if (element.name === "todo_user[email]" && element.value.includes('@') && element.value.includes('.')) {
                 element.classList.remove("is-invalid")
                 element.nextElementSibling.style.display = 'none';
                 element.nextElementSibling.innerHTML = "";
-                this.invalidInput = true;
+                validInputs.push(1)
             }
 
             if (element.name === "todo_user[password][first]" && element.value.length < 6) {
                 element.classList.add("is-invalid")
                 element.nextElementSibling.style.display = 'block';
                 element.nextElementSibling.innerHTML = "Your password must contains atleast 6 characters";
-                this.invalidInput = false;
                 firstPassword = element.value;
+                validInputs.push(0)
             } else if (element.name === "todo_user[password][first]" && element.value.length >= 6) {
                 firstPassword = element.value;
                 element.classList.remove("is-invalid")
                 element.nextElementSibling.style.display = 'none';
                 element.nextElementSibling.innerHTML = "";
-                this.invalidInput = true;
+                validInputs.push(1)
             }
 
             if (element.name === "todo_user[password][second]") {
-                if (element.value === firstPassword) {
+                if (element.value === firstPassword && firstPassword.length >= 6) {
                     element.classList.remove("is-invalid")
                     element.nextElementSibling.style.display = 'none';
                     element.nextElementSibling.innerHTML = "";
-                    this.invalidInput = true;
+                    validInputs.push(1)
                 } else if (firstPassword !== '') {
-                    this.invalidInput = false;
                     element.classList.add("is-invalid")
                     element.nextElementSibling.style.display = 'block';
                     element.nextElementSibling.innerHTML = "Your repeated password don't match your password. Please try again";
+                    validInputs.push(0)
                 }
+            }
+        })
+        validInputs.forEach(element => {
+            if (element === 0) {
+                this.invalidInput = true;
+            } else {
+                this.invalidInput = false;
             }
         })
     }
